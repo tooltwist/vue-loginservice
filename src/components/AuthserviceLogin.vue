04e20c91beda670394ba59b8815b683edaefc1b0
Philip Callender <philcal@mac.com>
2018-07-02 12:31:04 +0800

    Update

--- src/components/AuthserviceLogin.vue
+++ src/components/AuthserviceLogin.vue
@@ -122,7 +122,7 @@
         br
         | $login.user:
         br
-        .is-size-5
+        .card.is-size-7
           | {{$authservice.user}}
         br
         a(v-on:click="doSignout()") {{signin ? 'Sign out' : 'Logout'}}
